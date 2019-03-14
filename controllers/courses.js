d6162828e16b9e9e70252ff36ea2c3789561d99b
saketoz98 <saket.ozarkar@gmail.com>
2019-03-14 22:13:11 +0530

    Indiviual course dashboard added, Links unfinished

--- controllers/courses.js
+++ controllers/courses.js
@@ -13,24 +13,50 @@ exports.getCourses = (req,res)=>{
         });
     })
 }
-
-exports.getIndividualCourse = (req,res)=>{
-    const course_id = req.params.course_id;
-    Course.findById(course_id,(err,course)=>{
-
-        const syllabus = course.syllabus;
+const getCourseAtrributes = (course)=>{
+    const syllabus = course.syllabus;
         const courseName = course.courseName;
         const courseDesc = course.description;
         const modules = [];
         syllabus.forEach((module) => {
             modules.push(module);
         });
-        // console.log(modules[0].moduleName);
-        res.render('courses/individualCourse',{
+        return {
             courseName,
             courseDesc,
-            modules 
+            modules
+        }
+
+}
+exports.getIndividualCourse = (req,res)=>{
+    const course_id = req.params.course_id;
+    Course.findById(course_id,(err,course)=>{
+
+        const attr = getCourseAtrributes(course);
+        console.log(attr);
+        res.render('courses/individualCourse',{
+            course_id,
+            ...attr
         });
     })
 }
 
+exports.getSubtopic = (req,res)=>{
+    const course_id = req.params.course_id;
+    const moduleId = req.params.moduleId;
+    const subtopicId = req.params.subtopicId;
+
+    Course.findById(course_id,(err,course)=>{
+        if(err){
+            console.log(err);
+        }
+        const subtopicDetails = course.syllabus[moduleId].subtopics[subtopicId];
+        console.log(subtopicDetails);
+        const attr = getCourseAtrributes(course);
+        res.render('courses/subtopicDetails',{
+            course_id,
+            subtopicDetails,
+            ...attr
+        })
+    })
+}
