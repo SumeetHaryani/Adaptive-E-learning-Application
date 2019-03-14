cce2a936cda41ef87a16cc9cbac99a6c5b0a1762
Sugam Devare <sugamdevare@gmail.com>
2019-03-14 23:09:21 +0530

    survey added courses/:id/survey

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,28 +1,25 @@
-const mongoose = require('mongoose');
-const Course = require('../models/Course');
+const mongoose = require("mongoose");
+const Course = require("../models/Course");
 
-exports.getCourses = (req,res)=>{
-    
-    Course.find({},(err,courses)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log(courses);
-        res.render('courses/allCourses',{
-            allCourses : courses 
-        });
-    })
-}
+exports.getCourses = (req, res) => {
+  Course.find({}, (err, courses) => {
+    if (err) {
+      console.log(err);
+    }
+    console.log(courses);
+    res.render("courses/allCourses", {
+      allCourses: courses
+    });
+  });
+};
 
-exports.getIndividualCourse = (req,res)=>{
-    const course_id = req.params.course_id;
-    Course.findById(course_id,(err,course)=>{
-
-        const syllabus = course.syllabus
-
-        res.render('courses/individualCourse',{
-            course:course,
-            syllabus 
-        })
-    })
-}
+exports.getIndividualCourse = (req, res) => {
+  const course_id = req.params.course_id;
+  Course.findById(course_id, (err, course) => {
+    const syllabus = course.syllabus;
+    res.render("courses/individualCourse", {
+      course: course,
+      syllabus
+    });
+  });
+};
--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,13 @@
-const express = require('express');
-const CourseController = require('../controllers/courses');
+const express = require("express");
+const CourseController = require("../controllers/courses");
+const SurveyController = require("../controllers/survey");
 const router = express.Router();
 
 router.get("/courses", CourseController.getCourses);
 
 router.get("/courses/:course_id", CourseController.getIndividualCourse);
 
+router.get("/courses/:course_id/survey", SurveyController.survey);
+router.post("/courses/:course_id/survey", SurveyController.submit);
 // router.post("/potholes/:uid/:complaint_id",potholeController.changeStatus);
 module.exports = router;
