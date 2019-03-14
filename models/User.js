4af8671d425ca21bc29313ad74d76434be44a5c3
Sugam Devare <sugamdevare@gmail.com>
2019-03-14 20:00:26 +0530

    survey db added

--- models/User.js
+++ models/User.js
@@ -2,17 +2,28 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String,
-    firstName : String,
-    lastName : String,
-    courses : [
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Course",
-            status : String
-        }
-    ]
+  username: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  courses: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      status: String
+    }
+  ],
+
+  survey: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Course",
+      level: String,
+      goal: String,
+      outcome: String,
+      workExperience: String
+    }
+  ]
 });
 
 UserSchema.plugin(passportLocalMongoose);
