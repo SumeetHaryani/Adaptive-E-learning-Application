1d469cae2194e100936f7a904a0eae22e6e5e86e
SumeetHaryani <sumeetharyani3@gmail.com>
2019-03-15 07:32:08 +0530

    added youtube video

--- models/Course.js
+++ models/Course.js
@@ -2,31 +2,26 @@ var mongoose = require("mongoose");
 
 var CourseSchema = new mongoose.Schema({
     courseName: String,
-    description : String,
-    syllabus : [
-        {
-            moduleId: String,
-            moduleName : String,
-            subtopics : [
-                {
-                    subtopicName : String,
-                    videoLink : String,
-                    content : String
-                }
+    description: String,
+    syllabus: [{
+        moduleId: String,
+        moduleName: String,
+        subtopics: [{
+                subtopicName: String,
+                videolink: String,
+                content: String
+            }
 
-            ],
-            test : [
-                {
-                    questionId : String,
-                    questionDescription : String,
-                    difficulty : String,
-                    accuracy : Number,
-                    options : [],
-                    correctAnswer : String
-                }
-            ]
-        }
-    ]
+        ],
+        test: [{
+            questionId: String,
+            questionDescription: String,
+            difficulty: String,
+            accuracy: Number,
+            options: [],
+            correctAnswer: String
+        }]
+    }]
 });
 
 
