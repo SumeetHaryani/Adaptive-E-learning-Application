068afb4b3fe69b6962bd7b6979c1c452be9ddd69
Sugam Devare <sugamdevare@gmail.com>
2019-03-15 11:26:43 +0530

    analysis

--- controllers/courses.js
+++ controllers/courses.js
@@ -43,83 +43,98 @@ exports.getSubtopic = (req, res) => {
   const moduleId = req.params.moduleId;
   const subtopicId = req.params.subtopicId;
 
-        const attr = getCourseAtrributes(course);
-        console.log(attr);
-        res.render('courses/individualCourse',{
-            course_id,
-            ...attr
-        });
+  const attr = getCourseAtrributes(course);
+  console.log(attr);
+  res.render('courses/individualCourse', {
+    course_id,
+    ...attr
+  });
 }
 
 
-exports.getSubtopic = (req,res)=>{
-    const course_id = req.params.course_id;
-    const moduleId = req.params.moduleId;
-    const subtopicId = req.params.subtopicId;
+exports.getSubtopic = (req, res) => {
+  const course_id = req.params.course_id;
+  const moduleId = req.params.moduleId;
+  const subtopicId = req.params.subtopicId;
 
-    Course.findById(course_id,(err,course)=>{
-        if(err){
-            console.log(err);
-        }
-        const subtopicDetails = course.syllabus[moduleId].subtopics[subtopicId];
-        console.log(subtopicDetails);
-        const attr = getCourseAtrributes(course);
-        res.render('courses/subtopicDetails',{
-            course_id,
-            subtopicDetails,
-            ...attr
-        })
+  Course.findById(course_id, (err, course) => {
+    if (err) {
+      console.log(err);
+    }
+    const subtopicDetails = course.syllabus[moduleId].subtopics[subtopicId];
+    console.log(subtopicDetails);
+    const attr = getCourseAtrributes(course);
+    res.render('courses/subtopicDetails', {
+      course_id,
+      subtopicDetails,
+      ...attr
     })
+  })
 }
 
-exports.getQuiz = (req,res)=>{
-    const course_id = req.params.course_id;
-    const moduleId = req.params.moduleId;
+exports.getQuiz = (req, res) => {
+  const course_id = req.params.course_id;
+  const moduleId = req.params.moduleId;
 
-    Course.findById(course_id,(err,course)=>{
-        if(err){
-            console.log(err);
-        }
-        const test = course.syllabus[moduleId].test;
-        console.log(test);
-        const attr = getCourseAtrributes(course);
+  Course.findById(course_id, (err, course) => {
+    if (err) {
+      console.log(err);
+    }
+    const test = course.syllabus[moduleId].test;
+    console.log(test);
+    const attr = getCourseAtrributes(course);
 
-        res.render('courses/quiz',{
-            course_id,
-            moduleId,
-            ...attr,
-            test
-        })
+    res.render('courses/quiz', {
+      course_id,
+      moduleId,
+      ...attr,
+      test
     })
+  })
 
 }
 
-exports.postQuiz = (req,res)=>{
-    console.log("In Post");
-    console.log(req.body);
-    const course_id = req.params.course_id;
-    const moduleId = req.params.moduleId;
+exports.postQuiz = (req, res) => {
+  console.log("In Post");
+  console.log(req.body);
+  const course_id = req.params.course_id;
+  const moduleId = req.params.moduleId;
 
-    const answers = req.body;
-    Course.findById(course_id,(err,course)=>{
-        if(err){
-            console.log(err);
-        }
-        result = {};
-        const test = course.syllabus[moduleId].test;
-        test.forEach((question,index)=>{
-            if(question.correctAnswer === answers['question'+index] ){
-                console.log("Right");
-                result[index+1] = true
-            }else{
-                result[index+1] = false
+  const answers = req.body;
+  Course.findById(course_id, (err, course) => {
+    if (err) {
+      console.log(err);
+    }
+    result = [];
+    const test = course.syllabus[moduleId].test;
+    console.log("TEST:");
 
-            }
+    console.log(test);
+
+    test.forEach((question, index) => {
+      if (question.correctAnswer === answers["question" + index]) {
+        console.log("Right");
+        result.push({
+          outcome: "true",
+          question: question,
+          answer: question.options[question.correctAnswer],
+          user_answer: question.options[answers["question" + index]]
         });
-        console.log(result)
-        res.render("courses/resultAnalysis",{
-            result
+      } else {
+        result.push({
+          outcome: "false",
+          question: question,
+          answer: question.options[question.correctAnswer],
+          user_answer: question.options[answers["question" + index]]
         });
-    })
-    
-}
+      }
+    });
+    console.log("RESULT:");
+    console.log(result);
+    res.render("courses/resultAnalysis", {
+      result
+    });
+  });
+};
+
+
